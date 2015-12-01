a7fa43749ffc719af9fc3aa00ffb752652bd972f
Philip Cesar Garay <philipgaray2@gmail.com>
2015-12-02 00:32:06 +0800

    added more data and images for quiz

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,26 @@
 'use strict';
 
-angular.module('starter', ['ionic','ionic-audio','ngDraggable'])
+angular.module('starter', ['ionic', 'ionic-audio', 'ngDraggable'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+        $ionicPlatform.ready(function() {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
 
-  $ionicPlatform.onHardwareBackButton(function() {
-     event.preventDefault();
-     event.stopPropagation();
-  });
-})
-.config(function($ionicConfigProvider){
-  $ionicConfigProvider.backButton.text('').icon('ion-chevron-left').previousTitleText(false);;
-});
+        $ionicPlatform.onHardwareBackButton(function() {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    })
+    .config(function($ionicConfigProvider) {
+        $ionicConfigProvider.backButton.text('').icon('ion-chevron-left').previousTitleText(false);;
+    })
+    .constant('LETRA_GAME', ['B', 'D', 'A', 'P', 'M', 'S', 'O', 'D', 'M', 'T', 'A', 'I', 'O', 'P', 'S', 'I']);
